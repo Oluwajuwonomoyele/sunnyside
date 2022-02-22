14240c8ebf6ac52648d7bbec3fd2be2ef98ffec7
Oluwajuwon Olutomisin <jolutomisin@gmail.com>
2022-02-22 01:53:50 +0100

    add navbar toggle functionality

---
+++ main.js
@@ -0,0 +1,17 @@
+// navbar click event
+const openNav = document.querySelector('.menu-btn');
+const menu = document.querySelector('.mobile-menu')
+const closeNav = document.querySelector('.menu-btn i')
+const navLink = document.querySelectorAll('.nav-link')
+
+openNav.addEventListener('click', e => {
+    menu.classList.toggle('active');
+    closeNav.classList.toggle('active');
+});
+
+navLink.forEach( e => {
+    e.addEventListener('click', () => {
+        menu.classList.remove('active');
+        closeNav.classList.remove('active')
+    })
+})
