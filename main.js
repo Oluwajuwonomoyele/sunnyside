a7c75a5fc663e263d8c9f8c1667d57f55d61d167
Oluwajuwon Olutomisin <jolutomisin@gmail.com>
2022-02-22 13:49:34 +0100

    add fixed navbar

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // navbar click event
 const openNav = document.querySelector('.menu-btn');
-const menu = document.querySelector('.mobile-menu')
-const closeNav = document.querySelector('.menu-btn i')
-const navLink = document.querySelectorAll('.nav-link')
+const menu = document.querySelector('.mobile-menu');
+const closeNav = document.querySelector('.menu-btn i');
+const navLink = document.querySelectorAll('.nav-link');
 
 openNav.addEventListener('click', e => {
     menu.classList.toggle('active');
     closeNav.classList.toggle('active');
 });
 
+//onclick navlink to close navbar
 navLink.forEach( e => {
     e.addEventListener('click', () => {
         menu.classList.remove('active');
         closeNav.classList.remove('active')
-    })
-})
+    });
+});
